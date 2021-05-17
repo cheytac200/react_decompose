b4402b076446f806b9d9395db83be816e5e87281
Sasha Chornyi <sasha-202@inbox.ru>
2021-05-17 18:13:53 +0300

    Updates

---
+++ precache-manifest.6d7c83394bb932789bc20d51f452a521.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626ae429e05a23e17813a2f694a48b4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ff3c763789ab64669e39",
+    "url": "./static/css/main.70ca2857.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3c763789ab64669e39",
+    "url": "./static/js/main.2603118e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
